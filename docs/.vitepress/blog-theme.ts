5aa55d112590a0c64568b9111c52f9a8608704b6
徐浩杰 <xuhaojie3775@163.com>
2025-03-22 01:33:42 +0800

    个人化

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -6,9 +6,9 @@ import { getThemeConfig } from '@sugarat/theme/node'
 
 // const baseUrl = 'https://sugarat.top'
 // const RSS: Theme.RSSOptions = {
-//   title: '粥里有勺糖',
+//   title: 'XHAO',
 //   baseUrl,
-//   copyright: 'Copyright (c) 2018-present, 粥里有勺糖',
+//   copyright: 'Copyright (c) 2018-present, XHAO',
 //   description: '你的指尖,拥有改变世界的力量（大前端相关技术分享）',
 //   language: 'zh-cn',
 //   image: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
@@ -31,7 +31,7 @@ const blogTheme = getThemeConfig({
   footer: {
     // message 字段支持配置为HTML内容，配置多条可以配置为数组
     // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
-    copyright: 'MIT License | 粥里有勺糖',
+    copyright: 'MIT License | XHAO',
     // icpRecord: {
     //   name: '蜀ICP备19011724号',
     //   link: 'https://beian.miit.gov.cn/'
@@ -46,17 +46,10 @@ const blogTheme = getThemeConfig({
   themeColor: 'el-blue',
 
   // 文章默认作者
-  author: '粥里有勺糖',
+  author: 'XHAO',
 
   // 友链
   friend: [
-    {
-      nickname: '粥里有勺糖',
-      des: '你的指尖用于改变世界的力量',
-      avatar:
-        'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
-      url: 'https://sugarat.top',
-    },
     {
       nickname: 'Vitepress',
       des: 'Vite & Vue Powered Static Site Generator',
@@ -70,33 +63,29 @@ const blogTheme = getThemeConfig({
   popover: {
     title: '公告',
     body: [
-      { type: 'text', content: '👇公众号👇---👇 微信 👇' },
-      {
-        type: 'image',
-        src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210~fmt.webp'
-      },
-      {
-        type: 'text',
-        content: '欢迎大家加群&私信交流'
-      },
-      {
-        type: 'text',
-        content: '文章首/文尾有群二维码',
-        style: 'padding-top:0'
-      },
-      {
-        type: 'button',
-        content: '作者博客',
-        link: 'https://sugarat.top'
-      },
-      {
-        type: 'button',
-        content: '加群交流',
-        props: {
-          type: 'success'
-        },
-        link: 'https://theme.sugarat.top/group.html',
-      }
+      // { type: 'text', content: '👇公众号👇---👇 微信 👇' },
+      // {
+      //   type: 'text',
+      //   content: '欢迎大家加群&私信交流'
+      // },
+      // {
+      //   type: 'text',
+      //   content: '文章首/文尾有群二维码',
+      //   style: 'padding-top:0'
+      // },
+      // {
+      //   type: 'button',
+      //   content: '作者博客',
+      //   link: 'https://sugarat.top'
+      // },
+      // {
+      //   type: 'button',
+      //   content: '加群交流',
+      //   props: {
+      //     type: 'success'
+      //   },
+      //   link: 'https://theme.sugarat.top/group.html',
+      // }
     ],
     duration: 0
   },
